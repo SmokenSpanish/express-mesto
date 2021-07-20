91312085c4be7f92ab4561ebab9249cb7f7b6bae
Spanish <maximbasov1992@mail.ru>
2021-07-21 00:57:06 +0300

    finCheck13

--- app.js
+++ app.js
@@ -13,18 +13,23 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
 });
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', router);
+
 app.use('/', (req, res, next) => {
   res.status(404).send({ message: 'ресурс не найден' });
+
+  next();
+});
+
+app.use('/', (req, res, next) => {
   req.user = {
     _id: '60ec3488cbfd96361c425053',
   };
 
   next();
 });
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use('/', router);
 
 app.listen(PORT, () => {});
